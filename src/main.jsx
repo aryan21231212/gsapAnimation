f61c4acf33530cf81bf1474c54168c17bfc18bf7
aryanuu21 <aryanpratapsingh674@gmail.com>
2025-07-23 19:37:45 +0530

    changes made

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Noob from './Noob.jsx'
-import Practice from './practice.jsx'
+import Practice from './Practice.jsx'
 
 
 createRoot(document.getElementById('root')).render(
